af51327320b21c14e10cdb74d47719ea7fbd3fd9
Guy Carlos Villette <carlosvillette@gmail.com>
2022-01-20 22:08:21 -0500

    added post method to repo

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
+const bodyParser = require('body-parser');
 const  port = 3000;
 
 
 const app = express();
+app.use(bodyParser.json());
 const blockchain = new Blockchain();
 
-app.get('/api/blocks', (req,res) => {
+app.get('/api/blockchain', (req,res) => {
     res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req,res) => {
+    const data = req.body;
+    blockchain.addBlock(data);
+    res.json({
+        note: `Block ${blockchain.chain.length} has been mined successfully`,
+        block: blockchain.chain[blockchain.chain.length - 1]
+    });
 });
 
 app.listen(port, () => {
