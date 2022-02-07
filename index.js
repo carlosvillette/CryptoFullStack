7971b850cd84f2a1052d057ba9b0094bada41e3c
Guy Carlos Villette <carlosvillette@gmail.com>
2022-02-06 22:11:25 -0500

    allowed new nodes to automatically sync with root node

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
+const request = require('request');
+
 const PubSub = require('./pubsub');
-const  port = 3000;
+const Blockchain = require('./blockchain');
 
 const app = express();
-app.use(bodyParser.json());
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 pubsub.connect();
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
+app.use(bodyParser.json());
 
 app.get('/api/blockchain', (req,res) => {
     res.json(blockchain.chain);
@@ -27,7 +32,17 @@ app.post('/api/mine', (req,res) => {
     });
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blockchain`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT ==='true') {
@@ -38,4 +53,6 @@ if (process.env.GENERATE_PEER_PORT ==='true') {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
+
+    syncChains();
 });
