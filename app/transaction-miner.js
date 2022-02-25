79e70321d8c0aef2ee109fcb70b3eb8b07def7de
Guy Carlos Villette <carlosvillette@gmail.com>
2022-02-25 00:13:13 -0500

    added mine-transactions api

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
 
     constructor({blockchain, transactionPool, wallet, pubsub}) {
@@ -9,14 +11,20 @@ class TransactionMiner {
 
     mineTransaction() {
         // get the transaction pool's valid transaction
+        const validTransactions = this.transactionPool.validTransactions();
 
         // generate the miner's reward
+        const minerReward = Transaction.rewardTransaction({minerWallet: this.wallet});
 
         // add a block with theses transactions to the blockchain
+        validTransactions.push(minerReward);
+        this.blockchain.addBlock({data: validTransactions});
 
         // broadcast the updated blockchain
+        this.pubsub.broadcastChain();
 
         // clear the pool
+        this.transactionPool.clear();
     }
 }
 
--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const PubSub = require('./app/pubsub');
 const Blockchain = require('./blockchain/blockchain');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet/wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({blockchain, transactionPool});
+const transactionMiner = new TransactionMiner({blockchain,transactionPool,wallet,pubsub});
 
 const DEFAULT_PORT = 3000;
 
@@ -62,6 +64,12 @@ app.post('/api/transact', (req,res) => {
 
 app.get('/api/transaction-pool-map', (req,res) => {
     res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req,res) => {
+    transactionMiner.mineTransaction();
+
+    res.redirect('/api/blockchain');
 });
 
 const syncChains = () => {
