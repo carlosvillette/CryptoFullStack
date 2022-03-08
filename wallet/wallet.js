b4eb63c120057258a50b4dc82351fec4e8bdabf0
Guy Carlos Villette <carlosvillette@gmail.com>
2022-03-07 22:23:51 -0500

    Assured balance is calculated before each transaction

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -16,7 +16,14 @@ class Wallet {
         return signature;
     }
 
-    createTransaction({amount, recipient}) {
+    createTransaction({amount, recipient, chain}) {
+        if (chain) {
+            this.balance = Wallet.calculateBalance({
+                chain,
+                address: this.publicKey
+            })
+        }
+
         if (amount > this.balance) {
             throw new Error('Amount exceeded balance');
         }
