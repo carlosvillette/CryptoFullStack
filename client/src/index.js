5b8da60c302c77cd87b79cd746e357583845e7f7
Guy Carlos Villette <carlosvillette@gmail.com>
2022-03-15 21:16:21 -0400

    improved frontend development workflow

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from 'react-dom';
 
-render(<div>Cryptochain in React!</div>,
+render(<div>Cryptochain in React!: New Update 3</div>,
     document.getElementById('root')
 );
